80c8fb8eccda7513dfa81f729d857f4747319f70
Tomasz <tm.machine36@icloud.com>
2026-08-23 14:52:41 +0200

    Dodano menu aplikacji

---
+++ main.py
@@ -0,0 +1,15 @@
+print("=== MOJA APLIKACJA ===")
+print("1 - Przywitaj się")
+print("2 - Pokaż informację")
+print("3 - Zakończ")
+
+wybor = input("Wybierz opcję: ")
+
+if wybor == "1":
+    print("Witaj! Miło Cię widzieć.")
+elif wybor == "2":
+    print("To jest mój pierwszy program w Pythonie.")
+elif wybor == "3":
+    print("Do zobaczenia!")
+else:
+    print("Nieprawidłowa opcja.")
